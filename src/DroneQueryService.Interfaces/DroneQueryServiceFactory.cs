4b72610d308032405880d596a9847007c1771752
Joni Collinge <jonathan-collinge@hotmail.com>
2016-06-09 08:58:51 +0100

    Refactored sln structure

---
+++ src/DroneQueryService.Interfaces/DroneQueryServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneQueryService.Interfaces
+{
+    public class DroneQueryServiceFactory
+    {
+        private static string DRONEQUERYSERVICE_URI = "fabric:/Drones/DroneQueryService";
+        public static IDroneQueryService CreateDroneQueryService()
+        {
+            return ServiceProxy.Create<IDroneQueryService>(new Uri(DRONEQUERYSERVICE_URI), new ServicePartitionKey(0));
+        }
+    }
+}
