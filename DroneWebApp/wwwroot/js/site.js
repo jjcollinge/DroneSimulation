3ee927c79518a5959668434a991bb3527512bf71
Joni Collinge <jonathan-collinge@hotmail.com>
2016-07-05 15:07:49 +0100

    Added ajax instead of static refresh

--- DroneWebApp/wwwroot/js/site.js
+++ DroneWebApp/wwwroot/js/site.js
@@ -1,23 +1,27 @@
-﻿function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
+﻿var drones = [];
 
-var drones = httpGetAsync("http://localhost:8680/api/drone", function (dronesAsJsonString) {
-    console.log(dronesAsJsonString);
+(function worker() {
+    $.get("http://localhost:8680/api/drone", function (data) {
+        for (var i = 0; i < data.length; i++)
+        {
+            var drone = data[i];
+            var index = drones.indexOf(drone.Id);
+            if(index === -1) 
+            {
+                console.log(drone.Id + ' is a new id');
+                // Drone doesn't exist
+                $('.DroneList').append('<li><div class=drone' + drone.Id + '>Id: ' + drone.Id + ', Altitude: ' + drone.State.Altitude + ', Longitude: ' + drone.State.Longitude + ', Latitude: ' + drone.State.Latitude + '</div></li>');
+                // Store index
+                drones.push(drone.Id);
+            }
+            else
+            {
+                console.log(index + ' is an existing id');
+                // Drone exists
+                $('.drone' + drone.Id).html('Id: ' + drone.Id + ', Altitude: ' + drone.State.Altitude + ', Longitude: ' + drone.State.Longitude + ', Latitude: ' + drone.State.Latitude);
+            }
+        }
 
-    var drones = JSON.parse(dronesAsJsonString);
-    for(var i = 0; i < drones.length; i++)
-    {
-        console.log("Id: " + drones[i].Id);
-        console.log("Drones: " + drones[i].State);
-    }
-
-});
-
-setTimeout(window.location.reload(), 1000);
+        setTimeout(worker, 1000);
+    });
+})();
